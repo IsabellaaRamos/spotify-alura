2bccad6324e7a2f5692639bef7306ca08aa79fdd
Isabellaaramos <Isabellaramos7@outlook.com>
2025-01-31 11:03:15 -0300

    adiciona função de requisição à API para busca de artistas e ajusta visibilidade dos resultados

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const searchInput = document.getElementById('search-input');
 const resultsArtists = documentFragment.getElementById('result-artist');
 const resultPlaylists = document.getElementById('result-playlist');
 
+function requestApi(searchTerm) {
+    const url = "htpp://localhost:3000/artists?name_like=${searchTerm}";
+    fetch(url)
+    .then((response) => response.json())
+    .then((result) => displayResults(result));
+}
+
 
 document.addEventListener('input',function() {
     const  searchTerm = searchInput.value.toLowerCase();
     if (searchterm === '') {
+        resultPlaylists.classList.add('hidden');
+        resultsArtists.classList.remove('hidden');
+        return;
     
     }
 })
